a9835e61cce585cbdab86566777e62f6efaf7c8c
MADNA <madnamv@gmail.com>
2023-12-26 15:37:04 -0400

    ok

---
+++ lib/widgets/language_builder.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:multi_language_json/multi_language_json.dart';
+
+class LanguageBuilder extends StatelessWidget {
+  final Widget child;
+  LanguageBuilder({super.key, required this.child});
+  static late LangSupport langData;
+
+  @override
+  Widget build(BuildContext context) {
+    final locale = WidgetsFlutterBinding.ensureInitialized().window.locale;
+
+    MultiLanguageBloc language = MultiLanguageBloc(
+        initialLanguage: locale.languageCode,
+        defaultLanguage: locale.languageCode,
+        commonRoute: 'common',
+        supportedLanguages: ['en', 'es']);
+
+    return MultiLanguageStart(
+        future: language.init(),
+        builder: (context) => MultiStreamLanguage(builder: (context, data) {
+              langData = data;
+              return child;
+            }));
+  }
+}
