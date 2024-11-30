4b0760c8ce2ac5e45a33b86bfb0c689cf15b9c51
MADNA <madnamv@gmail.com>
2024-11-30 12:40:55 -0400

    trying to fix notifications

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -41,6 +41,7 @@ class NotificationService {
       },
     );
 
+  
     await AwesomeNotifications().setListeners(
       onActionReceivedMethod: onActionReceivedMethod,
       onNotificationCreatedMethod: onNotificationCreatedMethod,
@@ -54,6 +55,65 @@ class NotificationService {
       
     // );
 
+  }
+
+  static Future<bool> displayNotificationRationale() async {
+    bool userAuthorized = false;
+    BuildContext context = MyApp.navigatorKey.currentContext!;
+    await showDialog(
+        context: context,
+        builder: (BuildContext ctx) {
+          return AlertDialog(
+            title: Text('Get Notified!',
+                style: Theme.of(context).textTheme.titleLarge),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Image.asset(
+                        'assets/animated-bell.gif',
+                        height: MediaQuery.of(context).size.height * 0.3,
+                        fit: BoxFit.fitWidth,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                    'Allow Awesome Notifications to send you beautiful notifications!'),
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(ctx).pop();
+                  },
+                  child: Text(
+                    'Deny',
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge
+                        ?.copyWith(color: Colors.red),
+                  )),
+              TextButton(
+                  onPressed: () async {
+                    userAuthorized = true;
+                    Navigator.of(ctx).pop();
+                  },
+                  child: Text(
+                    'Allow',
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge
+                        ?.copyWith(color: Colors.deepPurple),
+                  )),
+            ],
+          );
+        });
+    return userAuthorized &&
+        await AwesomeNotifications().requestPermissionToSendNotifications();
   }
 
   /// Use this method to detect when a new notification or a schedule is created
@@ -73,8 +133,8 @@ class NotificationService {
       ReceivedAction receivedAction) async {
     debugPrint('onDismissActionReceivedMethod');
   }
-
   /// Use this method to detect when the user taps on a notification or action button
+  @pragma('vm:entry-point')
   static Future<void> onActionReceivedMethod(
       ReceivedAction receivedAction) async {
     debugPrint('onActionReceivedMethod');
@@ -120,6 +180,9 @@ class NotificationService {
     final int? interval,
   }) async {
     assert(!scheduled || (scheduled && interval != null));
+    bool isAllowed = await AwesomeNotifications().isNotificationAllowed();
+    if (!isAllowed) isAllowed = await displayNotificationRationale();
+    if (!isAllowed) return;
 
      AwesomeNotifications().createNotification(
       content: NotificationContent(
