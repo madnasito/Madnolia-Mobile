e21fd4ddb708c00fc7ad50148fb5de81737492d8
MADNA <madnamv@gmail.com>
2024-12-27 17:54:56 -0400

    Add: Text field molecule with form builder

--- lib/widgets/molecules/form/molecule_text_form_field.dart
+++ lib/widgets/molecules/form/molecule_text_form_field.dart
@@ -1,34 +1,49 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 import '../../../style/form_style.dart';
 
 
 class MoleculeTextField extends StatelessWidget {
-  final IconData? icon;
-  final String placeholder;
+  final String name;
+  final String label;
   final TextInputType keyboardType;
+  final GlobalKey<FormBuilderState> formKey;
   final bool isPassword;
-  final Key formKey;
+  final IconData? icon;
   final String? Function(String?)? validator;
-  final Function(String) onChanged;
-  const MoleculeTextField({super.key, this.icon, required this.placeholder, required this.keyboardType, required this.isPassword, required this.onChanged, this.validator, required this.formKey});
+  const MoleculeTextField( {
+    super.key,
+    required this.formKey,
+    required this.name,
+    required this.label,
+    this.keyboardType = TextInputType.text,
+    this.icon,
+    this.validator,
+    this.isPassword = false,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      key: formKey,
-      keyboardType: keyboardType,
-      decoration: InputDecoration(
-        prefixIcon: icon != null ? Icon(icon) : null,
-        hintText: placeholder,
-        errorBorder: errorBorder,
-        // enabledBorder: snapshot.hasData ? validBorder : null,
-        focusedErrorBorder: warningBorder,
-        focusedBorder: focusedBorder,
+    return FadeIn(
+      duration: const Duration(milliseconds: 300),
+      child: FormBuilderTextField(
+        autofocus: false,
+        name: name,
+        keyboardType: keyboardType,
+        obscureText: isPassword,
+        decoration: InputDecoration(
+          prefixIcon: icon != null ? Icon(icon) : null,
+          labelText: label,
+          errorBorder: errorBorder,
+          border: defaultBorder,
+          focusedErrorBorder: warningBorder,
+          focusedBorder: focusedBorder,
         ),
-      autovalidateMode: AutovalidateMode.always,
-      validator: validator,
-
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        validator: validator,
+      ),
     );
   }
 }
