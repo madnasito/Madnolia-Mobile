d8440d594857276fb9ebc0443cb8dae140cd6951
MADNA <madnamv@gmail.com>
2025-10-26 07:00:11 -0400

    Fix: receive updatedAt in null

--- lib/models/chat/chat_message_model.dart
+++ lib/models/chat/chat_message_model.dart
@@ -17,7 +17,7 @@ class ChatMessage {
     String conversation;
     String creator;
     DateTime date;
-    DateTime updatedAt;
+    DateTime? updatedAt;
 
     ChatMessage({
         required this.id,
@@ -27,7 +27,7 @@ class ChatMessage {
         required this.conversation,
         required this.creator,
         required this.date,
-        required this.updatedAt,
+        this.updatedAt,
     });
 
     factory ChatMessage.fromJson(Map<String, dynamic> json) {
@@ -75,7 +75,7 @@ class ChatMessage {
         conversation: json["conversation"],
         creator: json["creator"],
         date: DateTime.parse(json["date"]),
-        updatedAt: DateTime.parse(json["updatedAt"]),
+        updatedAt: json.containsKey("updatedAt") ? DateTime.parse(json["updatedAt"]) : null,
       );
     }
 
@@ -87,7 +87,7 @@ class ChatMessage {
         "conversation": conversation,
         "creator": creator,
         "date": date.toIso8601String(),
-        "updatedAt": updatedAt.toIso8601String(),
+        "updatedAt": updatedAt?.toIso8601String(),
     };
 
     ChatMessageCompanion toCompanion() {
